e2ecaff4cda706b82032130562837b04024f1f32
RootControl <lh.root.control@gmail.com>
2023-11-15 18:17:46 +0000

    created user and task models

---
+++ TaskManagement.API/Models/Task.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManagement.API;
+public class Task
+{
+    public Task(string title)
+    {
+        Id = Guid.NewGuid();
+        Created = DateTime.Now;
+        Title = title;
+    }
+
+    public Task(Guid? userId, string title, string? description, DateTime? deadline)
+    {
+        Id = Guid.NewGuid();
+        Created = DateTime.Now;
+        UserId = userId;
+        Title = title;
+        Description = description;
+        Deadline = deadline;
+    }    
+
+    [Key]
+    public Guid Id { get; private set; }
+
+    public DateTime Created { get; private set; }
+
+    public DateTime? CompletedDate { get; private set; }
+
+    public bool IsCompleted { get; private set; }
+
+    [ForeignKey(nameof(UserId))]
+    public Guid? UserId { get; private set; }
+
+    [Required]
+    public string Title { get; private set; }
+    
+    public string? Description { get; private set; }
+
+    public DateTime? Deadline { get; private set; }
+
+    public void Complete()
+    {
+        CompletedDate = DateTime.Now;
+        IsCompleted = true;
+    }
+
+    public void Update(Guid? userId, string title, string? description, DateTime? deadline)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty", nameof(title));
+
+        if (deadline.HasValue && deadline.Value < DateTime.Now)
+            throw new ArgumentException("Deadline cannot be in the past", nameof(deadline));
+
+        Title = title;
+        Description = description;
+        Deadline = deadline;
+        UserId = userId;
+    }
+
+    public void AssignTo(Guid userId)
+    {
+        UserId = userId;
+    }
+
+    public void Unassign()
+    {
+        UserId = null;
+    }
+}
---
+++ TaskManagement.API/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.API;
+public class User
+{
+    public User(string name, string email)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;
+    }
+
+    [Key]
+    public Guid Id { get; private set; }
+
+    [Required]
+    public string Name { get; private set; }
+
+    [Required]
+    public string Email { get; private set; }
+
+    public void Update(string name, string email)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name cannot be empty", nameof(name));
+
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Email cannot be empty", nameof(email));
+
+        Name = name;
+        Email = email;
+    }
+}
